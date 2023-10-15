8d1457e93d769d37d09e09c45291a8aa8db8386c
Parcelero88 <112039559+Parcelero88@users.noreply.github.com>
2023-10-15 16:17:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,37 @@ const game = () => {
   let pScore = 0;
   let cScore = 0;
 
-//Start
+  //Start the Game
   const startGame = () => {
-  const playBtn = document.querySelector(".intro button");
-  const introScreen = document.querySelector(".intro");
-  const match = document.querySelector(".match");
-playBtn.addEventListener("click", () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
       introScreen.classList.add("fadeOut");
       match.classList.add("fadeIn");
     });
   };
-  
-//Play
-    const playMatch = () => {
+  //Play Match
+  const playMatch = () => {
     const options = document.querySelectorAll(".options button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
     const hands = document.querySelectorAll(".hands img");
 
-hands.forEach(hand => {
-    hand.addEventListener("animationend", function() {
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
         this.style.animation = "";
       });
     });
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
 
-//Computer Options
-    const computerOptions = ['rock', 'paper', 'scissors'];
     options.forEach(option => {
       option.addEventListener("click", function() {
-      });
-    });
-        
-//Computer Choice
-      const computerNumber = Math.floor(Math.random() * 3);
-      const computerChoice = computerOptions[computerNumber];
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
 
         setTimeout(() => {
           //Here is where we call compare hands
@@ -44,11 +41,7 @@ hands.forEach(hand => {
           playerHand.src = `./assets/${this.textContent}.png`;
           computerHand.src = `./assets/${computerChoice}.png`;
         }, 2000);
-     });
-    });
-  };
-
-//Animation
+        //Animation
         playerHand.style.animation = "shakePlayer 2s ease";
         computerHand.style.animation = "shakeComputer 2s ease";
       });
@@ -64,27 +57,27 @@ hands.forEach(hand => {
 
   const compareHands = (playerChoice, computerChoice) => {
     //Update Text
-    const winner = document.querySelector('.winner');
+    const winner = document.querySelector(".winner");
     //Checking for a tie
     if (playerChoice === computerChoice) {
-      winner.textContent = "It is a tie!";
+      winner.textContent = "It is a tie";
       return;
     }
-//Check for Rock
-    if (playerChoice === 'rock") {
-      if (computerChoice === 'scissors') {
-        winner.textContent = 'Player Wins';
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins";
         pScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = 'Computer Wins';
+        winner.textContent = "Computer Wins";
         cScore++;
         updateScore();
         return;
       }
     }
-//Check for Paper
+    //Check for Paper
     if (playerChoice === "paper") {
       if (computerChoice === "scissors") {
         winner.textContent = "Computer Wins";
@@ -98,7 +91,7 @@ hands.forEach(hand => {
         return;
       }
     }
-//Check for Scissors
+    //Check for Scissors
     if (playerChoice === "scissors") {
       if (computerChoice === "rock") {
         winner.textContent = "Computer Wins";
@@ -114,7 +107,7 @@ hands.forEach(hand => {
     }
   };
 
-//Is call all the inner function
+  //Is call all the inner function
   startGame();
   playMatch();
 };
